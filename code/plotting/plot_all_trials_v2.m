b741615364f95a74165ed4de081159ba78153915
airsickegret <calebsponheim@uchicago.edu>
2020-09-01 15:34:58 -0500

    CT3 and all-trials plotting

---
+++ code/plotting/plot_all_trials_v2.m
@@ -0,0 +1,17 @@
+function [] = plot_all_trials_v2(meta,data)
+
+%%
+colors = hsv(meta.optimal_number_of_states);
+figure; hold on
+
+for iTrial = 1:size(data,2)
+    trial_colors = zeros(length(data(iTrial).states_resamp),3);
+    trial_colors(~isnan(data(iTrial).states_resamp),:) = colors(data(iTrial).states_resamp(~isnan(data(iTrial).states_resamp)),:);
+    scatter(data(iTrial).x_smoothed,data(iTrial).y_smoothed,5,trial_colors,'filled')
+end
+title(strcat(subject,' ',strrep(task,'_',' '),' all trials position'));
+% legend([plots{state_present(2,state_present(1,:)>0)}]);
+saveas(gcf,strcat(figure_folder_filepath,'\',subject,task,num2str(num_states_subject),'states','_all_trials_position.png'));
+
+end
+
--- code/post-processing/segment_analysis_v2.m
+++ code/post-processing/segment_analysis_v2.m
@@ -1,5 +1,11 @@
 function [meta,data] = segment_analysis_v2(meta,data)
 
 
-
+segment_count = ones(meta.optimal_number_of_states,1);
+for iTrial = 1:size(data,2)
+    for iState = 1:meta.optimal_number_of_states
+        data_by_state(iState).
+        segment_count(iState) = segment_count(iState)+length(
+    end
+end
 end
