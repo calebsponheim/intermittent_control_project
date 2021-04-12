9e4430366ae697dc321677c103638dd027a2aa0a
airsickegret <calebsponheim@uchicago.edu>
2021-04-12 10:53:33 -0500

    finished training and decoding

--- code/python_switching_models/run_simple_hmm.py
+++ code/python_switching_models/run_simple_hmm.py
@@ -8,7 +8,7 @@ Created on Tue Feb  9 09:41:00 2021
 # import csv
 # from os import listdir
 # from os.path import isfile, join
-# import numpy as np
+import numpy as np
 from import_matlab_data import import_matlab_data
 from assign_trials_to_HMM_group import assign_trials_to_HMM_group
 from train_HMM_for_optimal_states import train_HMM_for_optimal_states
@@ -35,21 +35,56 @@ trial_classification = assign_trials_to_HMM_group(data,meta)
 
 #%% saving data
 
-# import pickle
-
-# data.trial_classification = trial_classification
-# data.meta = meta
-# data.folderpath = folderpath
-
-# with open('C:/Users/calebsponheim/Documents/git/intermittent_control_project/data/python_switching_models/hmm_data.pickle', 'wb') as f:
-#     pickle.dump(data, f)
-
 #%% Finding Optimal States
 
-hmm_storage,hmm_lls_storage,bin_sums,optimal_state_number = train_HMM_for_optimal_states(data,trial_classification,meta,bin_size)
+hmm_storage, hmm_lls_storage, bin_sums, bin_sums_select, optimal_state_number = train_HMM_for_optimal_states(data,trial_classification,meta,bin_size)
 
 #%% Separate out Test Data
 
+
+trind_test = [i for i, x in enumerate(
+    trial_classification) if x == "test"]
+testset = []
+# S = []
+# trial_count = 1
+for iTrial in range(len(trial_classification)):
+    S_temp = data.spikes[iTrial]
+    for iUnit in range(len(S_temp)):
+        temp = S_temp[iUnit]
+        temp_indices = np.arange(0, len(temp), bin_size)
+        temp_binned = [temp[i] for i in temp_indices]
+        if iTrial in trind_test:
+            if len(testset) <= iUnit:
+                testset.append(temp_binned)
+            else:
+                testset[iUnit].extend(temp_binned)
+
+# Okay now that we have the training trials in its own variable, we need to turn it into the right shape for training, presumably.
+
+for iUnit in range(len(testset)):
+    if iUnit == 0:
+        bin_sums_test = testset[iUnit]
+    else:
+        bin_sums_test = np.vstack(
+            (bin_sums_test, testset[iUnit]))
+        
+
 #%% Decoding Test Data using Optimal States
 
-decoded_test_data = hmm_storage[optimal_state_number-1].most_likely_states(data)
+decoded_test_data = []
+for iState in range(len(hmm_storage)):
+    decoded_test_data.append(hmm_storage[iState].most_likely_states(np.transpose(bin_sums_test)))
+    
+#%% Save
+
+import csv
+
+with open('decoded_test_data.csv', 'w') as f:
+    write = csv.writer(f)
+    
+    write.writerows(decoded_test_data)
+    
+with open('trial_classifiction.csv', 'w') as f:
+    write = csv.writer(f,delimiter=' ')
+    for iTrial in range(len(trial_classification)):
+        write.writerow(trial_classification[iTrial])
--- code/python_switching_models/train_HMM_for_optimal_states.py
+++ code/python_switching_models/train_HMM_for_optimal_states.py
@@ -61,33 +61,50 @@ def train_HMM_for_optimal_states(data, trial_classification, meta, bin_size):
 
     # time_bins = bin_sums.shape[1]
     observation_dimensions = bin_sums.shape[0]
-    N_iters = 50
-    state_range = np.arange(1, 25)
-    # state_range = np.arange(1, 5)
+    N_iters = 30
+    state_range = np.arange(2, 26, 1)
+    # state_range = np.arange(1,5)
     bin_sums = bin_sums.astype(np.int64)
 
     hmm_storage = []
     hmm_lls_storage = []
     hmm_lls_max = []
     select_ll = []
-
+    AIC = []
+    
     for iState in state_range:
         hmm = ssm.HMM(iState, observation_dimensions, observations="poisson")
         hmm_storage.append(hmm)
         hmm_lls = hmm.fit(np.transpose(bin_sums), method="em",
                           num_iters=N_iters, init_method="kmeans")
-        select_ll.append(hmm.log_likelihood(np.transpose(bin_sums_select)))
         hmm_lls_storage.append(hmm_lls)
         hmm_lls_max.append(max(hmm_lls))
+        
+        # modele selection decode
+        select_ll_temp = hmm.log_likelihood(np.transpose(bin_sums_select))
+        select_ll.append(select_ll_temp)
+       
+        #AIC Section
+        if iState == 1:
+            num_params = (len(hmm.params[0][0]) + len(hmm.params[1][0]) + len(hmm.params[2]))
+        else:
+            num_params = (len(hmm.params[0][0]) + (len(hmm.params[1][0]) * len(hmm.params[1][0])) + (len(hmm.params[2]) * len(hmm.params[2][1])))
+        
+        LL = select_ll_temp
+        k = num_params
+        AIC.append(((-2)*(LL)) + (2 * (k)))
+        print(f'Created Model For {iState} States.')
 
     plt.plot(range(0,N_iters+1),np.transpose(hmm_lls_storage), label="EM")
     plt.xlabel("EM Iteration")
     plt.ylabel("Log Probability")
+    plt.title("Probability over iterations, with increasing state number")
     plt.show()
 
 
     plt.plot(state_range,np.transpose(select_ll))
     plt.xlabel("state number")
+    plt.title("Log Likelihood over state number")
     plt.ylabel("Log Probability")
     plt.show()
     # %% Determine Optimal State using "model classification" trials
@@ -96,7 +113,26 @@ def train_HMM_for_optimal_states(data, trial_classification, meta, bin_size):
 
 
     #%% AIC Calculation
-    
-    
-    
+   
+    # AIC with training LL
+    AIC_train = []
+    for iState in range(0,len(state_range)):
+        LL = hmm_lls_max[iState]
+        
+        if iState == 0:
+            num_params = (len(hmm_storage[iState].params[0][0]) + len(hmm_storage[iState].params[1][0]) + len(hmm_storage[iState].params[2]))
+        else:
+            num_params = (len(hmm_storage[iState].params[0][0]) + (len(hmm_storage[iState].params[1][0]) * len(hmm_storage[iState].params[1][0])) + (len(hmm_storage[iState].params[2]) * len(hmm_storage[iState].params[2][1])))
+        
+
+        k = num_params
+        AIC_train.append(((-2)*(LL)) + (2 * (k)))
+        
+    plt.plot(state_range,AIC_train)
+    plt.title("Akiake Information Criterion, with increasing state number")
+    plt.xlabel("state number")
+    plt.ylabel("AIC")
+    plt.show()
+
+    #%% Return variables
     return hmm_storage, hmm_lls_storage, bin_sums, bin_sums_select, optimal_state_number
