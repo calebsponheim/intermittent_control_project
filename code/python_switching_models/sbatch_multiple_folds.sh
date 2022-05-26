975624021009c41495c241232a173bb911664a20
Caleb <caleb.sponheim@gmail.com>
2022-05-26 15:56:31 -0500

    mid

--- code/python_switching_models/sbatch_multiple_folds.sh
+++ code/python_switching_models/sbatch_multiple_folds.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=rSLDS_1-100
 #SBATCH --output=/dali/nicho/caleb/git/intermittent_control_project/code/python_switching_models/rSLDS_1-100.out
 #SBATCH --error=/dali/nicho/caleb/git/intermittent_control_project/code/python_switching_models/rSLDS_1-100.err
-#SBATCH --time=72:00:00
+#SBATCH --time=24:00:00
 #SBATCH --partition=broadwl
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
