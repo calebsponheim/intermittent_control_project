11c3df17d1f611c0bfe3a0d149f8bc9da7f146d4
Caleb Scheffer Sponheim <calebsponheim@midway2-login1.rcc.local>
2022-05-31 11:19:54 -0500

    midway sync

--- code/python_switching_models/sbatch_multiple_folds.sh
+++ code/python_switching_models/sbatch_multiple_folds.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=rSLDS_1-100
+#SBATCH --job-name=CS_rSLDS
 #SBATCH --output=/dali/nicho/caleb/git/intermittent_control_project/code/python_switching_models/rSLDS_1-100.out
 #SBATCH --error=/dali/nicho/caleb/git/intermittent_control_project/code/python_switching_models/rSLDS_1-100.err
-#SBATCH --time=24:00:00
+#SBATCH --time=36:00:00
 #SBATCH --partition=broadwl
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
