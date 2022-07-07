e639b13dfd50a4ce76bbb8e0aa9eea40db6a04c0
Caleb Sponheim <caleb.sponheim@gmail.com>
2022-07-07 10:58:09 -0500

    ll_sync

--- code/python_switching_models/sbatch_files/sbatch_param_search_3_states.sh
+++ code/python_switching_models/sbatch_files/sbatch_param_search_3_states.sh
@@ -7,7 +7,7 @@
 #SBATCH --partition=broadwl
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=64G
+#SBATCH --mem-per-cpu=32G
 
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 echo "My SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
@@ -16,4 +16,4 @@ module load python/anaconda-2021.05
 
 source activate /dali/nicho/caleb/git/intermittent_control_project/data/ssm_midway_python_environment/
 
-python run_param_search.py $SLURM_ARRAY_TASK_ID 3
+python /dali/nicho/caleb/git/intermittent_control_project/code/python_switching_models/run_param_search.py $SLURM_ARRAY_TASK_ID 3
