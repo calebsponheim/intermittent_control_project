1b3339e1a9e9c899853586675712bfc208c5bcfb
Caleb <caleb.sponheim@gmail.com>
2022-07-15 13:36:37 -0500

    what

---
+++ data/python_switching_models/file.m
@@ -0,0 +1,2 @@
+filepath = 'C:\Users\Caleb (Work)\Documents\git\intermittent_control_project\data\python_switching_models\Bxcenter_out1803230.05sBins\Bxcenter_out_HMM_struct_22-Jun-2022.mat';
+data_export(filepath)
