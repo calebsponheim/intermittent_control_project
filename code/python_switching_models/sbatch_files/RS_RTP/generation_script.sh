620a3457a4b564cf65a54bc3787eaeaf5a38995f
Caleb <caleb.sponheim@gmail.com>
2022-09-27 11:07:40 -0500

    sync

--- code/python_switching_models/sbatch_files/RS_RTP/generation_script.sh
+++ code/python_switching_models/sbatch_files/RS_RTP/generation_script.sh
@@ -12,7 +12,7 @@ do
 		printf "#!/bin/bash\n#SBATCH --job-name=%i_%i\n#SBATCH --array=2-80:5\n" $iState $iFold > sbatch_dims_2-80_state_${iState}_fold_${iFold}.sh
 		printf "#SBATCH --output=/project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/out_files/%s_%s/rSLDS_%s_%i.out\n" $subject $taskname  "%a" $iState >> sbatch_dims_2-80_state_${iState}_fold_${iFold}.sh
 		printf "#SBATCH --output=/project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/error_files/%s_%s/rSLDS_%s_%i.err\n" $subject $taskname  "%a" $iState >> sbatch_dims_2-80_state_${iState}_fold_${iFold}.sh
-		printf "#SBATCH --time=36:00:00\n#SBATCH --mem-per-cpu=48G\n#SBATCH--account=pi-nicho\n#SBATCH --partition=caslake" >> sbatch_dims_2-80_state_${iState}_fold_${iFold}.sh
+		printf "#SBATCH --time=36:00:00\n#SBATCH --mem-per-cpu=48G\n#SBATCH --account=pi-nicho\n#SBATCH --partition=caslake\n" >> sbatch_dims_2-80_state_${iState}_fold_${iFold}.sh
 		printf "module load python/anaconda-2021.05\nsource activate /project/nicho/caleb/git/intermittent_control_project/data/ssm_midway_python_environment/\n" >> sbatch_dims_2-80_state_${iState}_fold_${iFold}.sh
 		printf "python /project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/run_param_search.py \$SLURM_ARRAY_TASK_ID %i %i %s %s" $iFold $iState $subject $taskname >> sbatch_dims_2-80_state_${iState}_fold_${iFold}.sh
 	done
--- code/python_switching_models/sbatch_files/RS_RTP/sbatch_dims_2-80_state_20_fold_3.sh
+++ code/python_switching_models/sbatch_files/RS_RTP/sbatch_dims_2-80_state_20_fold_3.sh
@@ -5,7 +5,8 @@
 #SBATCH --output=/project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/error_files/rs_RTP/rSLDS_%a_20.err
 #SBATCH --time=36:00:00
 #SBATCH --mem-per-cpu=48G
-#SBATCH--account=pi-nicho
-#SBATCH --partition=caslakemodule load python/anaconda-2021.05
+#SBATCH --account=pi-nicho
+#SBATCH --partition=caslake
+module load python/anaconda-2021.05
 source activate /project/nicho/caleb/git/intermittent_control_project/data/ssm_midway_python_environment/
 python /project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/run_param_search.py $SLURM_ARRAY_TASK_ID 3 20 rs RTP
