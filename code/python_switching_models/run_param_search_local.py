6a070acf7291c5033d2f934d1389766f86ff0f7c
Caleb <caleb.sponheim@gmail.com>
2022-10-05 09:25:15 -0500

    ugh

--- code/python_switching_models/run_param_search_local.py
+++ code/python_switching_models/run_param_search_local.py
@@ -1,4 +1,5 @@
 from run_rslds import run_rslds
+
 train_portion = 0.8
 model_select_portion = 0.0
 test_portion = 0.2
@@ -6,13 +7,13 @@ hidden_max_state_range = 120
 hidden_state_skip = 1
 
 
-latent_dim_state_range = 12
+number_of_latent_dimensions = 32
+number_of_discrete_states = 4
 fold_number = 1
 subject = 'rs'
-task = 'CO'
-num_hidden_state_override = 8
-rslds_ll_analysis = 0
-midway_run = 1
+task = 'RTP'
+midway_run = 0
+rslds_ll_analysis = midway_run
 num_neuron_folds = 4
 
 # %% Running it
@@ -24,9 +25,9 @@ run_rslds(
     test_portion,
     hidden_max_state_range,
     hidden_state_skip,
-    num_hidden_state_override,
+    number_of_discrete_states,
     rslds_ll_analysis,
-    latent_dim_state_range,
+    number_of_latent_dimensions,
     midway_run,
     fold_number,
     num_neuron_folds
