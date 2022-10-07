c8a181c6b92309d44af5d37c83c72a7af00b6a56
Caleb <caleb.sponheim@gmail.com>
2022-10-07 15:51:37 -0500

    sytnc

--- code/python_switching_models/sbatch_files/RS_CO_moveonly/sbatch_dims_2-80_state_18_fold_5.sh
+++ code/python_switching_models/sbatch_files/RS_CO_moveonly/sbatch_dims_2-80_state_18_fold_5.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=18_5
-#SBATCH --array=5-20
-#SBATCH --output=/dali/nicho/caleb/git/intermittent_control_project/code/python_switching_models/out_files/rs_CO/rSLDS_%a_18.out
-#SBATCH --output=/dali/nicho/caleb/git/intermittent_control_project/code/python_switching_models/error_files/rs_CO/rSLDS_%a_18.err
+#SBATCH --array=2-80:5
+#SBATCH --output=/project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/out_files/rs_CO/rSLDS_%a_18.out
+#SBATCH --output=/project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/error_files/rs_CO/rSLDS_%a_18.err
 #SBATCH --time=36:00:00
-#SBATCH --partition=broadwl
-#SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=48G
+#SBATCH --account=pi-nicho
+#SBATCH --partition=caslake
 module load python/anaconda-2021.05
-source activate /dali/nicho/caleb/git/intermittent_control_project/data/ssm_midway_python_environment/
-python /dali/nicho/caleb/git/intermittent_control_project/code/python_switching_models/run_param_search.py $SLURM_ARRAY_TASK_ID 5 18 rs CO
+source activate /project/nicho/caleb/git/intermittent_control_project/data/ssm_midway_python_environment/
+python /project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/run_param_search.py $SLURM_ARRAY_TASK_ID 5 18 rs CO
---
+++ code/python_switching_models/sbatch_files/RS_CO_moveonly/sbatch_dims_2-80_state_26_fold_3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=26_3
+#SBATCH --array=2-80:5
+#SBATCH --output=/project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/out_files/rs_CO/rSLDS_%a_26.out
+#SBATCH --output=/project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/error_files/rs_CO/rSLDS_%a_26.err
+#SBATCH --time=36:00:00
+#SBATCH --mem-per-cpu=48G
+#SBATCH --account=pi-nicho
+#SBATCH --partition=caslake
+module load python/anaconda-2021.05
+source activate /project/nicho/caleb/git/intermittent_control_project/data/ssm_midway_python_environment/
+python /project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/run_param_search.py $SLURM_ARRAY_TASK_ID 3 26 rs CO
