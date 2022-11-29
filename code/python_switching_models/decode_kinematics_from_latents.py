d3109726d727372a14197e6ab79599dc5dbce9a8
Caleb <caleb.sponheim@gmail.com>
2022-11-29 10:30:29 -0600

    decoding

--- code/python_switching_models/decode_kinematics_from_latents.py
+++ code/python_switching_models/decode_kinematics_from_latents.py
@@ -4,12 +4,22 @@ Created on Mon Nov 21 09:45:48 2022
 
 @author: caleb_work
 """
-import pandas as pd
-from os import listdir
-from os.path import isfile, join
 
 
-def decode_kinematics_from_latents(folderpath):
+import os
+
+
+def decode_kinematics_from_latents(kinpath, latentpath, model):
+
+    import pandas as pd
+    import numpy as np
+    from os import listdir
+    from os.path import isfile, join
+    import Neural_Decoding
+    import matplotlib.pyplot as plt
+    # from scipy import io
+    # from scipy import stats
+    # import pickle
     # Load Kinematics
     kinfiles = [
         f
@@ -23,7 +33,7 @@ def decode_kinematics_from_latents(folderpath):
     x_velocity_by_trial = []
     y_velocity_by_trial = []
     speed_by_trial = []
-
+    full_kinematics_by_trial = []
     for iFile in kinfiles:
         kinematics = pd.DataFrame.to_numpy(pd.read_csv(folderpath + iFile))
         x_by_trial.append(kinematics[:, 0])
@@ -31,19 +41,164 @@ def decode_kinematics_from_latents(folderpath):
         x_velocity_by_trial.append(kinematics[:, 2])
         y_velocity_by_trial.append(kinematics[:, 3])
         speed_by_trial.append(kinematics[:, 4])
-
+        full_kinematics_by_trial.append(kinematics)
         file_count += 1
         if file_count % 100 == 0:
             print(f"Processed Kinematics from trial {file_count}")
+
+    # Bin Kinematics
+    full_kinematics_binned = []
+    for iTrial in np.arange(len(full_kinematics_by_trial)):
+        vels = np.asarray(full_kinematics_by_trial[iTrial])
+        vel_times = np.arange(len(x_by_trial[iTrial]))/1000
+        dt = .05
+        t_start = 0
+        t_end = len(x_by_trial[iTrial])/1000
+        downsample_factor = 1
+        full_kinematics_binned.append(Neural_Decoding.bin_output(
+            vels, vel_times, dt, t_start, t_end, downsample_factor))
+
     # Load Latents
+    latentfiles = [
+        f
+        for f in listdir(folderpath)
+        if isfile(join(folderpath, f))
+        if f.startswith("latent_states_" + model + "_trial_")
+    ]
+    file_count = 0
+    latents_by_trial = []
 
-    infile = open(folderpath_out + 'fold_' + str(fold_number) + '_model', 'rb')
-    model = pickle.load(infile)
-    infile.close()
-    # Structure Latent Data to be timesteps x neurons, float64 array, non-numpy.
+    for iFile in latentfiles:
+        latents = pd.DataFrame.to_numpy(pd.read_csv(folderpath + iFile))
+        latents_by_trial.append(latents)
+        file_count += 1
+        if file_count % 100 == 0:
+            print(f"Processed Latents from trial {file_count}")
 
-    # Bin Kinematics into 50ms bins
+    # %% Decoding
+    training_range = [0, 0.7]
+    testing_range = [0.7, 0.85]
+    valid_range = [0.85, 1]
+    y_valid_predicted_kf_all = []
+    R2_kf = np.zeros([len(full_kinematics_by_trial), 5])
+    for iTrial in np.arange(len(full_kinematics_by_trial)):
+        lag = 0
+        X_kf = latents_by_trial[iTrial]
+        y_kf = full_kinematics_binned[iTrial]
 
-    # Structure Kinematics to be timesteps x kinematics
+        # Number of examples after taking into account bins removed for lag alignment
+        num_examples_kf = X_kf.shape[0]
 
-    #
+        # Note that each range has a buffer of 1 bin at the beginning and end
+        # This makes it so that the different sets don't include overlapping data
+        training_set = np.arange(int(np.round(
+            training_range[0]*num_examples_kf))+1, int(np.round(training_range[1]*num_examples_kf))-1)
+        testing_set = np.arange(int(
+            np.round(testing_range[0]*num_examples_kf))+1, int(np.round(testing_range[1]*num_examples_kf))-1)
+        valid_set = np.arange(int(
+            np.round(valid_range[0]*num_examples_kf))+1, int(np.round(valid_range[1]*num_examples_kf))-1)
+
+        # Get training data
+        X_kf_train = X_kf[training_set, :]
+        y_kf_train = y_kf[training_set, :]
+
+        # Get testing data
+        X_kf_test = X_kf[testing_set, :]
+        y_kf_test = y_kf[testing_set, :]
+
+        # Get validation data
+        X_kf_valid = X_kf[valid_set, :]
+        y_kf_valid = y_kf[valid_set, :]
+
+        # Z-score inputs
+        X_kf_train_mean = np.nanmean(X_kf_train, axis=0)
+        X_kf_train_std = np.nanstd(X_kf_train, axis=0)
+        X_kf_train = (X_kf_train-X_kf_train_mean)/X_kf_train_std
+        X_kf_test = (X_kf_test-X_kf_train_mean)/X_kf_train_std
+        X_kf_valid = (X_kf_valid-X_kf_train_mean)/X_kf_train_std
+
+        # Zero-center outputs
+        y_kf_train_mean = np.mean(y_kf_train, axis=0)
+        y_kf_train = y_kf_train-y_kf_train_mean
+        y_kf_test = y_kf_test-y_kf_train_mean
+        y_kf_valid = y_kf_valid-y_kf_train_mean
+
+        # Declare model
+        # There is one optional parameter that is set to the default in this example (see ReadMe)
+        model_kf = Neural_Decoding.KalmanFilterDecoder(C=1)
+
+        # Fit model
+        model_kf.fit(X_kf_train, y_kf_train)
+
+        # Get predictions
+        y_valid_predicted_kf = model_kf.predict(X_kf_valid, y_kf_valid)
+        y_valid_predicted_kf_all.append(y_valid_predicted_kf)
+        # Get metrics of fit (see read me for more details on the differences between metrics)
+        # First I'll get the R^2
+        R2_kf[iTrial, :] = Neural_Decoding.get_R2(y_kf_valid, y_valid_predicted_kf)
+        # I'm just printing the R^2's of the 3rd and 4th entries that correspond to the velocities
+        # Next I'll get the rho^2 (the pearson correlation squared)
+        # rho_kf = Neural_Decoding.get_rho(y_kf_valid, y_valid_predicted_kf)
+        # I'm just printing the rho^2's of the 3rd and 4th entries that correspond to the velocities
+
+        # As an example, I plot an example 1000 values of the x velocity (column index 2), both true and predicted with the Kalman filter
+        # Note that I add back in the mean value, so that both true and predicted values are in the original coordinates
+        if (iTrial % 100) == 0:
+            fig_x_kf = plt.figure()
+            plt.plot(y_kf_valid[:, 0]+y_kf_train_mean[0], y_kf_valid[:, 1]+y_kf_train_mean[1], 'b')
+            plt.plot(y_valid_predicted_kf[:, 0]+y_kf_train_mean[0],
+                     y_valid_predicted_kf[:, 1]+y_kf_train_mean[1], 'r')
+        # Save figure
+        # fig_x_kf.savefig('x_velocity_decoding.eps')
+    return R2_kf, y_valid_predicted_kf_all
+
+
+# %% Parameter Setting
+
+num_latent_dims_rslds = 25
+num_discrete_states_rslds = 10
+num_latent_dims_slds = 2
+num_discrete_states_slds = 2
+num_latent_dims_lds = 2
+num_discrete_states_hmm = 2
+subject = 'rs'
+task = 'RTP'
+model = 'rslds'
+
+# %% Data Import
+current_working_directory = os.getcwd()
+if "calebsponheim" in current_working_directory:
+    folderpath_base_base = "C:/Users/calebsponheim/Documents/git/intermittent_control_project/"
+elif "dali" in current_working_directory:
+    folderpath_base_base = "/dali/nicho/caleb/git/intermittent_control_project/"
+elif "Caleb (Work)" in current_working_directory:
+    folderpath_base_base = "C:/Users/Caleb (Work)/Documents/git/intermittent_control_project/"
+folderpath_base = folderpath_base_base + "data/python_switching_models/"
+
+if subject == "bx":
+    if task == "CO":
+        folderpath = folderpath_base + "Bxcenter_out1902280.05sBins/"
+    elif task == "CO+RTP":
+        folderpath = folderpath_base + "Bxcenter_out_and_RTP1902280.05sBins/"
+elif subject == "bx18":
+    folderpath = folderpath_base + "Bxcenter_out1803230.05sBins/"
+elif subject == "rs":
+    if task == "CO":
+        folderpath = folderpath_base + "RSCO_move_window0.05sBins/"
+    elif task == "RTP":
+        folderpath = folderpath_base + "RSRTP0.05sBins/"
+elif subject == "rj":
+    folderpath = folderpath_base + "RJRTP0.05sBins/"
+else:
+    print("BAD, NO")
+
+temp_folderlist = os.listdir(folderpath)
+if str(num_discrete_states_rslds) + "_states_" + str(num_latent_dims_rslds) + "_dims" not in temp_folderlist:
+    os.mkdir(folderpath + str(num_discrete_states_rslds) +
+             "_states_" + str(num_latent_dims_rslds) + "_dims/")
+    latentpath = (folderpath + str(num_discrete_states_rslds) +
+                  "_states_" + str(num_latent_dims_rslds) + "_dims/")
+
+kinpath = folderpath
+
+R2_kf, y_valid_predicted_kf_all = decode_kinematics_from_latents(kinpath, latentpath, model)
--- code/python_switching_models/model_performance_comparison.py
+++ code/python_switching_models/model_performance_comparison.py
@@ -77,6 +77,7 @@ temp_figurelist = os.listdir(figurepath)
 if str(num_discrete_states_rslds) + "_states_" + str(num_latent_dims_rslds) + "_dims" not in temp_folderlist:
     os.mkdir(folderpath + str(num_discrete_states_rslds) +
              "_states_" + str(num_latent_dims_rslds) + "_dims/")
+
 if str(num_discrete_states_rslds) + "_states_" + str(num_latent_dims_rslds) + "_dims" not in temp_figurelist:
     os.mkdir(figurepath + str(num_discrete_states_rslds) +
              "_states_" + str(num_latent_dims_rslds) + "_dims/")
@@ -124,185 +125,186 @@ elif 'multifold_trial_classification.csv' not in temp_datafolderlist:
     multifold_shuffled_order_out.to_csv(
         folderpath_out + 'multifold_trial_classification.csv', index=False, header=True)
 
-for iFold in np.arange(1, trial_folds+1):
-    # bring in which fold it is
-    # take that segment of data
-    fold_test_data_range_start = int((
-        (test_portion*iFold) - test_portion)*number_of_trials)
-    fold_test_data_range_end = int((test_portion*iFold)*number_of_trials)
-    test_mask = np.ones_like(trial_indices, bool)
-    test_mask[fold_test_data_range_start:fold_test_data_range_end] = False
-    test_mask = np.logical_not(test_mask)
-    train_mask = np.logical_not(test_mask)
-    fold_test_trials = multifold_shuffled_order[test_mask]
-    fold_train_trials = multifold_shuffled_order[train_mask]
-    trial_classification = []
-    for iTrial in range(number_of_trials):
-        if iTrial in fold_test_trials:
-            trial_classification.append('test')
-        elif iTrial in fold_train_trials:
-            trial_classification.append('train')
+iFold = 1
+# for iFold in np.arange(1, trial_folds+1):
+# bring in which fold it is
+# take that segment of data
+fold_test_data_range_start = int((
+    (test_portion*iFold) - test_portion)*number_of_trials)
+fold_test_data_range_end = int((test_portion*iFold)*number_of_trials)
+test_mask = np.ones_like(trial_indices, bool)
+test_mask[fold_test_data_range_start:fold_test_data_range_end] = False
+test_mask = np.logical_not(test_mask)
+train_mask = np.logical_not(test_mask)
+fold_test_trials = multifold_shuffled_order[test_mask]
+fold_train_trials = multifold_shuffled_order[train_mask]
+trial_classification = []
+for iTrial in range(number_of_trials):
+    if iTrial in fold_test_trials:
+        trial_classification.append('test')
+    elif iTrial in fold_train_trials:
+        trial_classification.append('train')
 
-    trind_train = [i for i, x in enumerate(trial_classification) if x == "train"]
-    trind_test = [i for i, x in enumerate(trial_classification) if x == "test"]
+trind_train = [i for i, x in enumerate(trial_classification) if x == "train"]
+trind_test = [i for i, x in enumerate(trial_classification) if x == "test"]
 
-    trainset = []
-    testset = []
+trainset = []
+testset = []
 
-    for iTrial in range(len(trial_classification)):
-        if iTrial in trind_train:
-            trainset.append(np.transpose(np.array(data.spikes[iTrial])))
-        elif iTrial in trind_test:
-            testset.append(np.transpose(np.array(data.spikes[iTrial])))
+for iTrial in range(len(trial_classification)):
+    if iTrial in trind_train:
+        trainset.append(np.transpose(np.array(data.spikes[iTrial])))
+    elif iTrial in trind_test:
+        testset.append(np.transpose(np.array(data.spikes[iTrial])))
 
-    observation_dimensions = trainset[0].shape[1]
+observation_dimensions = trainset[0].shape[1]
 
-    # # HMM
-    # N_iters = 100
-    # hmm = ssm.HMM(
-    #     num_discrete_states_hmm,
-    #     observation_dimensions,
-    #     observations="poisson",
-    #     transitions="standard",
-    # )
+# # HMM
+# N_iters = 100
+# hmm = ssm.HMM(
+#     num_discrete_states_hmm,
+#     observation_dimensions,
+#     observations="poisson",
+#     transitions="standard",
+# )
 
-    # hmm.fit(
-    #     trainset,
-    #     method="em",
-    #     num_iters=N_iters,
-    #     init_method="random",
-    # )
+# hmm.fit(
+#     trainset,
+#     method="em",
+#     num_iters=N_iters,
+#     init_method="random",
+# )
 
-    # # model selection decode
-    # hmm_test_ll.append(hmm.log_likelihood(testset))
+# # model selection decode
+# hmm_test_ll.append(hmm.log_likelihood(testset))
 
-    # print("Created HMM Model")
+# print("Created HMM Model")
 
-    # # LDS
-    # model = ssm.LDS(observation_dimensions, num_latent_dims_lds,
-    #                 emissions="poisson")
-    # model.initialize(trainset)
-    # q_elbos_lem_train, q_lem_train = model.fit(trainset, method="laplace_em",
-    #                                            variational_posterior="structured_meanfield",
-    #                                            num_iters=25)
+# # LDS
+# model = ssm.LDS(observation_dimensions, num_latent_dims_lds,
+#                 emissions="poisson")
+# model.initialize(trainset)
+# q_elbos_lem_train, q_lem_train = model.fit(trainset, method="laplace_em",
+#                                            variational_posterior="structured_meanfield",
+#                                            num_iters=25)
 
-    # inputs = inputs = [np.zeros((y.shape[0], model.M)) for y in testset]
-    # masks = []
-    # for y in testset:
-    #     mask = np.ones_like(y)
-    #     mask = mask.astype(bool)
-    #     masks.append(mask)
+# inputs = inputs = [np.zeros((y.shape[0], model.M)) for y in testset]
+# masks = []
+# for y in testset:
+#     mask = np.ones_like(y)
+#     mask = mask.astype(bool)
+#     masks.append(mask)
 
-    # _elbos, _q_model = model.approximate_posterior(
-    #     testset,
-    #     method="laplace_em",
-    #     variational_posterior="structured_meanfield",
-    #     num_iters=25, alpha=0.5)
+# _elbos, _q_model = model.approximate_posterior(
+#     testset,
+#     method="laplace_em",
+#     variational_posterior="structured_meanfield",
+#     num_iters=25, alpha=0.5)
 
-    # lls = 0.0
-    # for tr in range(len(testset)):
-    #     ll = np.sum(model.emissions.log_likelihoods(testset[tr],
-    #                                                 inputs[tr],
-    #                                                 mask=masks[tr],
-    #                                                 tag=None,
-    #                                                 x=_q_model.mean_continuous_states[tr]))
-    #     lls += ll
-    # lds_test_ll.append(lls)
-    # print("Created LDS Model")
+# lls = 0.0
+# for tr in range(len(testset)):
+#     ll = np.sum(model.emissions.log_likelihoods(testset[tr],
+#                                                 inputs[tr],
+#                                                 mask=masks[tr],
+#                                                 tag=None,
+#                                                 x=_q_model.mean_continuous_states[tr]))
+#     lls += ll
+# lds_test_ll.append(lls)
+# print("Created LDS Model")
 
-    # # SLDS
-    # model = ssm.SLDS(observation_dimensions, num_discrete_states_slds, num_latent_dims_slds,
-    #                  emissions="poisson",
-    #                  single_subspace=True)
-    # model.initialize(trainset)
-    # q_elbos_lem_train, q_lem_train = model.fit(trainset, method="laplace_em",
-    #                                            variational_posterior="structured_meanfield",
-    #                                            num_iters=25)
+# # SLDS
+# model = ssm.SLDS(observation_dimensions, num_discrete_states_slds, num_latent_dims_slds,
+#                  emissions="poisson",
+#                  single_subspace=True)
+# model.initialize(trainset)
+# q_elbos_lem_train, q_lem_train = model.fit(trainset, method="laplace_em",
+#                                            variational_posterior="structured_meanfield",
+#                                            num_iters=25)
 
-    # inputs = inputs = [np.zeros((y.shape[0], model.M)) for y in testset]
-    # masks = []
-    # for y in testset:
-    #     mask = np.ones_like(y)
-    #     mask = mask.astype(bool)
-    #     masks.append(mask)
+# inputs = inputs = [np.zeros((y.shape[0], model.M)) for y in testset]
+# masks = []
+# for y in testset:
+#     mask = np.ones_like(y)
+#     mask = mask.astype(bool)
+#     masks.append(mask)
 
-    # _elbos, _q_model = model.approximate_posterior(
-    #     testset,
-    #     method="laplace_em",
-    #     variational_posterior="structured_meanfield",
-    #     num_iters=25, alpha=0.5)
+# _elbos, _q_model = model.approximate_posterior(
+#     testset,
+#     method="laplace_em",
+#     variational_posterior="structured_meanfield",
+#     num_iters=25, alpha=0.5)
 
-    # lls = 0.0
-    # for tr in range(len(testset)):
-    #     ll = np.sum(model.emissions.log_likelihoods(testset[tr],
-    #                                                 inputs[tr],
-    #                                                 mask=masks[tr],
-    #                                                 tag=None,
-    #                                                 x=_q_model.mean_continuous_states[tr]))
-    #     lls += ll
-    # slds_test_ll.append(lls)
-    # print("Created SLDS Model")
+# lls = 0.0
+# for tr in range(len(testset)):
+#     ll = np.sum(model.emissions.log_likelihoods(testset[tr],
+#                                                 inputs[tr],
+#                                                 mask=masks[tr],
+#                                                 tag=None,
+#                                                 x=_q_model.mean_continuous_states[tr]))
+#     lls += ll
+# slds_test_ll.append(lls)
+# print("Created SLDS Model")
 
-    # rSLDS
-    model = ssm.SLDS(observation_dimensions, num_discrete_states_rslds, num_latent_dims_rslds,
-                     transitions="recurrent",
-                     dynamics="diagonal_gaussian",
-                     emissions="poisson",
-                     single_subspace=True)
-    model.initialize(trainset)
-    q_elbos_lem_train, q_lem_train = model.fit(trainset, method="laplace_em",
-                                               variational_posterior="structured_meanfield",
-                                               initialize=False,
-                                               num_iters=15)
+# rSLDS
+model = ssm.SLDS(observation_dimensions, num_discrete_states_rslds, num_latent_dims_rslds,
+                 transitions="recurrent",
+                 dynamics="diagonal_gaussian",
+                 emissions="poisson",
+                 single_subspace=True)
+model.initialize(trainset)
+q_elbos_lem_train, q_lem_train = model.fit(trainset, method="laplace_em",
+                                           variational_posterior="structured_meanfield",
+                                           initialize=False,
+                                           num_iters=15)
 
-    # Pickling/Saving Trained Model
+# Pickling/Saving Trained Model
 
-    filename = folderpath_out + 'fold_' + str(iFold) + '_model_pickle_for_performance_comparison'
-    outfile = open(filename, 'wb')
-    pickle.dump(model, outfile)
-    outfile.close()
+filename = folderpath_out + 'fold_' + str(iFold) + '_model_pickle_for_performance_comparison'
+outfile = open(filename, 'wb')
+pickle.dump(model, outfile)
+outfile.close()
 
-    q_elbos_lem_test, q_lem_test = model.approximate_posterior(
-        datas=testset,
-        method="laplace_em",
-        variational_posterior="structured_meanfield",
-        num_iters=15)
+q_elbos_lem_test, q_lem_test = model.approximate_posterior(
+    datas=testset,
+    method="laplace_em",
+    variational_posterior="structured_meanfield",
+    num_iters=15)
 
-    xhat_lem = []
-    for iTrial in range(len(trind_train)):
-        xhat_lem.insert(trind_train[iTrial], q_lem_train.mean_continuous_states[iTrial])
-    for iTrial in range(len(trind_test)):
-        xhat_lem.insert(trind_test[iTrial], q_lem_test.mean_continuous_states[iTrial])
+xhat_lem = []
+for iTrial in range(len(trind_train)):
+    xhat_lem.insert(trind_train[iTrial], q_lem_train.mean_continuous_states[iTrial])
+for iTrial in range(len(trind_test)):
+    xhat_lem.insert(trind_test[iTrial], q_lem_test.mean_continuous_states[iTrial])
 
-    for iTrial in range(len(xhat_lem)):
-        latent_states_rslds_temp = pd.DataFrame(xhat_lem[iTrial])
-        latent_states_rslds_temp.to_csv(folderpath_out + "latent_states_rslds_trial_" +
-                                        str(iTrial+1) + "_fold_" + str(iFold) + ".csv", index=False, header=False)
+for iTrial in range(len(xhat_lem)):
+    latent_states_rslds_temp = pd.DataFrame(xhat_lem[iTrial])
+    latent_states_rslds_temp.to_csv(folderpath_out + "latent_states_rslds_trial_" +
+                                    str('{:04}'.format(iTrial+1)) + "_fold_" + str(iFold) + ".csv", index=False, header=False)
 
-    inputs = inputs = [np.zeros((y.shape[0], model.M)) for y in testset]
-    masks = []
-    for y in testset:
-        mask = np.ones_like(y)
-        mask = mask.astype(bool)
-        masks.append(mask)
+inputs = inputs = [np.zeros((y.shape[0], model.M)) for y in testset]
+masks = []
+for y in testset:
+    mask = np.ones_like(y)
+    mask = mask.astype(bool)
+    masks.append(mask)
 
-    _elbos, _q_model = model.approximate_posterior(
-        testset,
-        method="laplace_em",
-        masks=masks,
-        variational_posterior="structured_meanfield",
-        num_iters=15, alpha=0.5)
+_elbos, _q_model = model.approximate_posterior(
+    testset,
+    method="laplace_em",
+    masks=masks,
+    variational_posterior="structured_meanfield",
+    num_iters=15, alpha=0.5)
 
-    lls = 0.0
-    for tr in range(len(testset)):
-        ll = np.sum(model.emissions.log_likelihoods(testset[tr],
-                                                    inputs[tr],
-                                                    mask=masks[tr],
-                                                    tag=None,
-                                                    x=_q_model.mean_continuous_states[tr]))
-        lls += ll
-    rslds_test_ll.append(lls)
-    print("Created rSLDS Model")
+lls = 0.0
+for tr in range(len(testset)):
+    ll = np.sum(model.emissions.log_likelihoods(testset[tr],
+                                                inputs[tr],
+                                                mask=masks[tr],
+                                                tag=None,
+                                                x=_q_model.mean_continuous_states[tr]))
+    lls += ll
+rslds_test_ll.append(lls)
+print("Created rSLDS Model")
 
 # %% Save Results
 
@@ -318,4 +320,3 @@ lds_test_ll_out.to_csv(
 rslds_test_ll_out = pd.DataFrame(rslds_test_ll)
 rslds_test_ll_out.to_csv(
     folderpath_out + 'rslds_test_ll_for_model_comparison.csv', index=False, header=True)
-a
