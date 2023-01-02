877ad794bda4eba8c72e12ea5a98ff29fdc85019
Caleb Sponheim <caleb.sponheim@gmail.com>
2023-01-02 12:49:14 -0600

    bx18 model done

--- code/python_switching_models/run_param_search_local.py
+++ code/python_switching_models/run_param_search_local.py
@@ -7,12 +7,12 @@ hidden_max_state_range = 120
 hidden_state_skip = 1
 
 
-number_of_latent_dimensions = 2
-number_of_discrete_states = 2
+number_of_latent_dimensions = 20
+number_of_discrete_states = 6
 fold_number = 1
 subject = 'bx18'
 task = 'CO'
-midway_run = 1
+midway_run = 0
 rslds_ll_analysis = midway_run
 num_neuron_folds = 4
 train_model = 1
