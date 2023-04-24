ecce804df796ae88e0d053a18f2d8083a2b6c1da
Caleb Sponheim <caleb.sponheim@gmail.com>
2023-04-24 11:01:18 -0500

    fixing filepaths

--- code/python_switching_models/sbatch_files/RS_RTP/lds/generation_script.sh
+++ code/python_switching_models/sbatch_files/RS_RTP/lds/generation_script.sh
@@ -8,9 +8,9 @@ for iFold in `seq 1 5`
 do
 	echo "$iFold"
 	printf "#!/bin/bash\n#SBATCH --job-name=f_%i\n#SBATCH --array=2-80\n" $iFold > sbatch_dims_2-80_fold_${iFold}.sh
-	printf "#SBATCH --output=/project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/out_files/%s_%s/LDS_%s_%s.out\n" $subject $taskname  "%a" $iFold >> sbatch_dims_2-80_fold_${iFold}.sh
-	printf "#SBATCH --output=/project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/error_files/%s_%s/LDS_%s_%s.err\n" $subject $taskname  "%a"  $iFold >> sbatch_dims_2-80_fold_${iFold}.sh
-	printf "#SBATCH --time=1:00:00\n#SBATCH --mem-per-cpu=48G\n#SBATCH --account=pi-nicho\n#SBATCH --partition=caslake\n" >> sbatch_dims_2-80_fold_${iFold}.sh
-	printf "module load python/anaconda-2021.05\nsource activate /project/nicho/caleb/git/intermittent_control_project/data/ssm_midway_python_environment/\n" >> sbatch_dims_2-80_fold_${iFold}.sh
-	printf "python /project/nicho/caleb/git/intermittent_control_project/code/python_switching_models/LDS_param_search.py %i %s %s \$SLURM_ARRAY_TASK_ID " $iFold $taskname $subject >> sbatch_dims_2-80_fold_${iFold}.sh
+	printf "#SBATCH --output=/project/nicho/projects/caleb/git/intermittent_control_project/code/python_switching_models/out_files/%s_%s/LDS_%s_%s.out\n" $subject $taskname  "%a" $iFold >> sbatch_dims_2-80_fold_${iFold}.sh
+	printf "#SBATCH --output=/project/nicho/projects/caleb/git/intermittent_control_project/code/python_switching_models/error_files/%s_%s/LDS_%s_%s.err\n" $subject $taskname  "%a"  $iFold >> sbatch_dims_2-80_fold_${iFold}.sh
+	printf "#SBATCH --time=10:00:00\n#SBATCH --mem-per-cpu=48G\n#SBATCH --account=pi-nicho\n#SBATCH --partition=caslake\n" >> sbatch_dims_2-80_fold_${iFold}.sh
+	printf "module load python/anaconda-2022.05\nsource activate /project/nicho/projects/caleb/git/intermittent_control_project/data/ssm_midway_python_environment/\n" >> sbatch_dims_2-80_fold_${iFold}.sh
+	printf "python /project/nicho/projects/caleb/git/intermittent_control_project/code/python_switching_models/LDS_param_search.py %i %s %s \$SLURM_ARRAY_TASK_ID " $iFold $taskname $subject >> sbatch_dims_2-80_fold_${iFold}.sh
 done
